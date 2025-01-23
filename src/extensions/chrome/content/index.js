e7ebcbde8d4d2eca044f369fff1f40e3b01c3be4
Shawn Flanigan <shawnmflanigan@gmail.com>
2025-01-23 07:21:50 -0800

    Chrome extension (#1)
    * boilerplate
    
    * basic script
    
    * works
    
    * refactor
    
    * refactor to classes
    
    * nit
    
    * nit
    
    * testing new line nit
    
    * remove password
    
    * add email
    
    * change class name
    
    * remove password input type detection
    
    * change name/description
    
    * drop manefest_version
    
    * drop version
    
    ---------
    
    Co-authored-by: Shawn Flanigan <shawnmflanign@gmail.com>

---
+++ src/extensions/chrome/content/index.js
@@ -0,0 +1,73 @@
+// TODO: FICUS!
+// browser.runtime.onMessage.addListener((message, sender, sendResponse) => {
+//   if (message.action === 'decorateAds') {
+//     const script = document.createElement('script');
+//     const src = `https://dev.savage.allyabase.com/game-scene.js?decoration=${message.decoration}`;
+//     console.log(src);
+//     script.src = src;
+//     console.log(script.src);
+//     document.body.appendChild(script);
+
+//     console.log(message.decoration);
+//     console.log('foo');
+//   }
+// });
+
+
+console.log('index loading')
+
+const detector = new InputDetector();
+const simulator = new TypingSimulator({
+  minDelay: 50,
+  maxDelay: 150,
+  naturalMode: true,
+});
+
+let histeresis = false;
+// TODO: PageObserver class
+// Also run detection when dynamic content is added
+const observer = new MutationObserver((mutations) => {
+  for (const mutation of mutations) {
+    if (!histeresis && mutation.addedNodes.length) {
+      histeresis = true;
+      setTimeout(() => {
+        histeresis = false;
+        console.log('histeresis changed to ', histeresis);
+      }, 1500);
+      detector.detectFields();
+    }
+  }
+});
+
+observer.observe(document.body, {
+  childList: true,
+  subtree: true,
+});
+
+(() => {
+  setTimeout(() => {
+    // TODO:
+    // console.log('adding script');
+    // const script = document.createElement('script');
+    // script.src = `http://127.0.0.1:5117/game-scene.js`;
+    // script.src = `https://dev.savage.allyabase.com/game-scene.js`;
+    // document.body.appendChild(script);
+
+    // Run detection when page loads
+    console.log('running detect fields');
+    detector.detectFields();
+
+    document.addEventListener('click', async (event) => {
+      // Get clicked element info
+      const element = event.target;
+      console.log(element);
+      if (element.type === 'email') {
+        element.focus();
+        //element.value = "letstest@planetnineapp.com";
+        const email = 'letstest@planetnineapp.com';
+        await simulator.typeIntoElement(element, email);
+        event.preventDefault();
+      }
+    });
+  }, 3000);
+})();
---
+++ src/extensions/chrome/content/scripts/TypingSimulator.js
@@ -0,0 +1,106 @@
+class TypingSimulator {
+    constructor(options = {}) {
+      this.minDelay = options.minDelay || 50; // Minimum delay between keystrokes
+      this.maxDelay = options.maxDelay || 150; // Maximum delay between keystrokes
+      this.naturalMode = options.naturalMode !== false; // Add small variations in timing
+    }
+  
+    async typeIntoElement(element, text) {
+      // Focus the element first
+      element.focus();
+  
+      // Clear existing value if needed
+      if (element.value) {
+        element.value = '';
+        this.triggerEvent(element, 'input');
+      }
+  
+      // Type each character
+      for (let i = 0; i < text.length; i++) {
+        const char = text[i];
+  
+        // Get keyboard event details for this character
+        const keyDetails = this.getKeyDetails(char);
+  
+        // Simulate keydown
+        this.triggerKeyEvent(element, 'keydown', keyDetails);
+  
+        // Simulate keypress
+        this.triggerKeyEvent(element, 'keypress', keyDetails);
+  
+        // Update input value to include the new character
+        element.value = text.substring(0, i + 1);
+  
+        // Simulate input event
+        this.triggerEvent(element, 'input');
+  
+        // Simulate keyup
+        this.triggerKeyEvent(element, 'keyup', keyDetails);
+  
+        // Wait for a random delay before next character
+        await this.delay();
+      }
+  
+      // Trigger final change event
+      this.triggerEvent(element, 'change');
+  
+      // Blur the element
+      element.blur();
+      this.triggerEvent(element, 'blur');
+    }
+  
+    getKeyDetails(char) {
+      return {
+        key: char,
+        code: `Key${char.toUpperCase()}`,
+        keyCode: char.charCodeAt(0),
+        which: char.charCodeAt(0),
+        shiftKey: /[A-Z]/.test(char),
+        bubbles: true,
+        cancelable: true,
+      };
+    }
+  
+    triggerKeyEvent(element, eventType, keyDetails) {
+      const event = new KeyboardEvent(eventType, {
+        ...keyDetails,
+        view: window,
+        composed: true,
+      });
+  
+      // Handle special key properties that can't be set through the constructor
+      Object.defineProperties(event, {
+        keyCode: { value: keyDetails.keyCode },
+        which: { value: keyDetails.which },
+        key: { value: keyDetails.key },
+      });
+  
+      element.dispatchEvent(event);
+    }
+  
+    triggerEvent(element, eventType) {
+      const event = new Event(eventType, {
+        bubbles: true,
+        cancelable: true,
+        composed: true,
+      });
+      element.dispatchEvent(event);
+    }
+  
+    delay() {
+      let delay = Math.random() * (this.maxDelay - this.minDelay) + this.minDelay;
+  
+      if (this.naturalMode) {
+        // Add occasional longer pauses
+        if (Math.random() < 0.1) {
+          delay *= 2;
+        }
+  
+        // Add slight variations
+        delay += (Math.random() - 0.5) * 25;
+      }
+  
+      return new Promise((resolve) => setTimeout(resolve, delay));
+    }
+  }
+  
